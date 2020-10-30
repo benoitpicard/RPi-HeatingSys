51bf39f4fb8322d7949d7e3243f8b664cfe59552
benoitpicard <picard.benoit@gmail.com>
2020-10-30 00:06:35 -0400

    Update tempRead.py

--- tempRead.py
+++ tempRead.py
@@ -12,15 +12,15 @@ import adafruit_dht # source: https://learn.adafruit.com/dht-humidity-sensing-on
 
 # SENSOR LIST (DS18B20)
 # RPi pin no default in used (GPIO4) for 1-wire protocol
-TS1_Name=('TA_U','TF_U','TA_M','TF_M','TW_IN','TW_OUT')
-TS1_Unit=('C','C','C','C','C','C')
-TS1_ID=('01144bf1efaa',
+TS1_Name=['TA_U','TF_U','TA_M','TF_M','TW_IN','TW_OUT']
+TS1_Unit=['C','C','C','C','C','C']
+TS1_ID=['01144bf1efaa',
     '01145167b9aa',
     '000005675be3',
     '01144b8b70aa',
     '0114515ff8aa',
     '0114515740aa'
-    )
+    ]
 TS1_Count=len(TS1_Name)
 
 # DS18B20 SETUP
@@ -29,8 +29,8 @@ for iS in range(TS1_Count):
     DS18B20_SENS.append(W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, TS1_ID[iS]))
 
 # DHT SETUP
-TS2_Name=('TA_UD','HA_UD','TA_MD','HA_MD')
-TS2_Unit=('C','%','C','%')
+TS2_Name['TA_UD','HA_UD','TA_MD','HA_MD']
+TS2_Unit=['C','%','C','%']
 TS2_Count=len(TS2_Name)
 DHT_SENS=[]
 DHT_SENS.append(adafruit_dht.DHT11(board.D12))
@@ -65,8 +65,8 @@ while True:
                 continue
     
     # GROUP DATA WITH DATE
-    TS_NAME=('Date')+TS1_Name+TS2_Name
-    TS_Unit=('')+TS1_Unit+TS2_Unit
+    TS_NAME=['Date']+TS1_Name+TS2_Name
+    TS_Unit=['']+TS1_Unit+TS2_Unit
     TS_Data=[pd.to_datetime('today')]+TS1_Data+TS2_Data
     
     #Write to CSV
