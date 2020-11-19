34c038fc685fc3ae10cc11ceb077c0d4565ee7c9
Benoit Picard <benoit.picard@usherbrooke.ca>
2020-11-19 16:11:21 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+file_valveCmd="../../RPi-HeatingSys-Data/valveCmd.csv"
+read_valveCmd=pd.read_csv(file_valveCmd)
+valveName=['V1U','V2M','V3G','V4E']
+outStr=str('[%.19s] ' % pd.to_datetime('today'))
+for iP in range(4):
+	#Get Relay Status
+	pinNo=RelayPinNo[iP]
+	valveCmd=read_valveCmd.loc[0,valveName[iP]]
+	outStr=outStr+valveName[iP]+ '_' + valveCmd
+    return outStr
+
+@app.route('/test')
+def index():
+    return 'test1'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
