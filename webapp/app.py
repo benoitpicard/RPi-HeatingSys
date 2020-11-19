b358d1ecff630db4f54d65c4d78d1623f0c5465c
Benoit Picard <benoit.picard@usherbrooke.ca>
2020-11-19 16:11:53 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-file_valveCmd="../../RPi-HeatingSys-Data/valveCmd.csv"
-read_valveCmd=pd.read_csv(file_valveCmd)
-valveName=['V1U','V2M','V3G','V4E']
-outStr=str('[%.19s] ' % pd.to_datetime('today'))
-for iP in range(4):
-	#Get Relay Status
-	pinNo=RelayPinNo[iP]
-	valveCmd=read_valveCmd.loc[0,valveName[iP]]
-	outStr=outStr+valveName[iP]+ '_' + valveCmd
-    return outStr
+	file_valveCmd="../../RPi-HeatingSys-Data/valveCmd.csv"
+	read_valveCmd=pd.read_csv(file_valveCmd)
+	valveName=['V1U','V2M','V3G','V4E']
+	outStr=str('[%.19s] ' % pd.to_datetime('today'))
+	for iP in range(4):
+		#Get Relay Status
+		pinNo=RelayPinNo[iP]
+		valveCmd=read_valveCmd.loc[0,valveName[iP]]
+		outStr=outStr+valveName[iP]+ '_' + valveCmd
+		return outStr
 
 @app.route('/test')
 def index():
