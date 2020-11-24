fddbd0b3e5e6a2f6dadbef934cdf0853d096ef56
benoitpicard <picard.benoit@gmail.com>
2020-11-23 22:15:36 -0500

    data image loading

--- utilitiesHSC.py
+++ utilitiesHSC.py
@@ -1,4 +1,6 @@
 import os, sys, traceback, time
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Extract Temperature setpoint from pandas structure dataframe
 def getSetpointTemp(dfSetpoint,Zone,nowDateTime,typeDayRef,pd):
@@ -7,7 +9,7 @@ def getSetpointTemp(dfSetpoint,Zone,nowDateTime,typeDayRef,pd):
     # Slice Setpoint DataFrame by Week/End and Zone
     DayRef=typeDayRef[nowDateTime.weekday()]
     dfSS=dfSetpoint[(dfSetpoint['Day']==DayRef) &
-        (dfSetpoint['Zone']==Zone)]
+        (dfSetpoint['Zone']==Zone)].copy()
     # Convert to timedelta for easy comparison
     dfSS['Time']=pd.to_timedelta(dfSS['Time'])
     nowTimeD=pd.to_timedelta(nowDateTime-pd.Timestamp.normalize(nowDateTime))
@@ -24,8 +26,7 @@ def getSetpointTemp(dfSetpoint,Zone,nowDateTime,typeDayRef,pd):
     return TA, TF
 
 # Read CSV with pandas with try/except loop
-def tryReadCSV(file_name,index,pd):
-    attemptCount=5
+def tryReadCSV(file_name,index,pd,attemptCount=5,parseCol=''):
     for attempt in range(attemptCount):
         try:
             #read csv with pandas and replace index with date
@@ -44,3 +45,54 @@ def tryReadCSV(file_name,index,pd):
             errorActive=True
             time.sleep(0.1)
     return readCSV, errorActive
+
+# Read CSV with pandas with try/except loop, with parsing  
+def tryReadCSV_p(file_name,index,pd,attemptCount=5,parseCol=''):
+    for attempt in range(attemptCount):
+        try:
+            #read csv with pandas and replace index with date
+            if parseCol!='':
+                readCSV=pd.read_csv(file_name,parse_dates=[parseCol])
+            if index!='':
+                readCSV=readCSV.set_index(index)
+            errorActive=False
+            break
+        except:
+            #retry reading (sometime fails due to simulatneous read/write callback)
+            print('[%.19s] %s: error reading file_tempSensor (attempt#%d)' % 
+                (pd.to_datetime('today'),sys.argv[0],attempt))
+            traceback.print_exc(file=sys.stdout)
+            if attempt<attemptCount-1:
+                print('   --- continuing ---')
+            errorActive=True
+            time.sleep(0.1)
+    return readCSV, errorActive
+    
+# Generate fig
+def genFigHHMM(df,xList,yLists,xLabel,yLabels):
+    # Sizing def
+    SMALL_SIZE = 12
+    MEDIUM_SIZE = 14
+    BIGGER_SIZE = 16
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    figInfo=[]
+    for iFig in range(len(yLists)):
+        subPlotCnt=len(yLists[iFig])
+        fig, axs = plt.subplots(subPlotCnt,1,figsize=(8,10)) #figsize 12,8 makes it 1200x800 pixels
+        myFmt = mdates.DateFormatter('%H:%M')
+        for iSP in range(subPlotCnt):
+            df.plot(x=xList[iFig], y=yLists[iFig][iSP], ax=axs[iSP], legend=True)
+            axs[iSP].xaxis.set_major_formatter(myFmt)
+        axs[0].set_xlabel('')
+        axs[1].set_xlabel('Time of day')
+        axs[0].set_ylabel('Temperature (C)')
+        axs[1].set_ylabel('On/Off (1/0)')
+        fig.tight_layout()
+        figInfo.append(fig)
+    return figInfo
--- webApp.py
+++ webApp.py
@@ -1,18 +1,12 @@
 # Import main modules
 from flask import Flask, render_template
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-
-
-# Import main modules
-import time
-import numpy as np
-import pandas as pd
-import os, sys, traceback
 
 # Import code functions
 from utilitiesHSC import tryReadCSV
+from utilitiesHSC import tryReadCSV_p
+from utilitiesHSC import genFigHHMM
 
 # Web server application 
 app = Flask(__name__)
@@ -65,6 +59,34 @@ def home():
     currentData_HTML=currentData.transpose().to_html(border=1,index=True,header=False,classes='w3-table w3-striped w3-border')
     return render_template('home.html',tableHTML_1=currentData_HTML,currentTime=timeStr)
 
+
+@app.route('/data')
+def data():
+
+    # Get latest data:
+    nowDateTime=pd.to_datetime('today')
+    fileDay=nowDateTime.strftime('%Y%m%d')
+    file_controlSys='../RPi-HeatingSys-Data/DATA/'+fileDay+'_HSC_Data.csv'
+    read_controlSys,errorActive=tryReadCSV_p(file_controlSys,'',pd,5,'DateTime')
+    read_controlSys['TW_dT (C)']=read_controlSys['TW_IN (C)']-read_controlSys['TW_OUT (C)']
+    read_controlSys['TW_dTon (C)']=read_controlSys['TW_dT (C)']
+    read_controlSys.loc[(read_controlSys['V1U']==0) &
+                        (read_controlSys['V2M']==0) & 
+                        (read_controlSys['V3G']==0),'TW_dTon (C)']=np.nan
+                        
+    xList=['DateTime','DateTime','DateTime']
+    yLists=[[['TA_M (C)','TF_M (C)','TA_M_TG (C)'],['V2M']], 
+            [['TA_U (C)','TF_U (C)','TA_U_TG (C)'],['V1U']],
+            [['TW_IN (C)','TW_OUT (C)'],['TW_dT (C)','TW_dTon (C)']]]
+    figObj=genFigHHMM(read_controlSys,xList,yLists,'','')
+    filePath='./static/'
+    figPath=[]
+    for iFO in range(len(figObj)):
+        figPath.append(filePath+'plot'+str(iFO+1)+'.jpg')
+        figObj[iFO].savefig(figPath[iFO]) #to save to local folder 
+    
+    timeStr=nowDateTime.strftime('%H:%M:%S')
+    return render_template('data.html',imgs=figPath,currentTime=timeStr)
 
 @app.route('/<name>')
 def blank(name):
